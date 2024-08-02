4770b64f4f28a1c8b39dd7d970deb8f3b07e6bf4
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-02 21:17:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(getenv('API_ID', 25833520))
 API_HASH = str(getenv('API_HASH', '7d012a6cbfabc2d0436d7a09d8362af7'))
-BOT_TOKEN = str(getenv('BOT_TOKEN' , '6738059809:AAFfYft25qo-iKP-XpgLgq9ZCyhKoew8994'))
+BOT_TOKEN = str(getenv('BOT_TOKEN' , '6383263721:AAGH6i2QcQYUrrTJXD-HSZX-7YNSCnwpuYc'))
 START_IMG = "https://graph.org/file/8dc72eae13be09ff294ef.jpg"
 REPEAT_IMG = "https://graph.org/file/e913a80094926b1bb9778.jpg"
 CHANNEL_LINKS = ["https://t.me/channel1", "https://t.me/channel2", "https://t.me/channel3"]
