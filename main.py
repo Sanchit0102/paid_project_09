0773701d3f1ea0a111e568073a7e0294f05521e2
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-02 11:34:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def start(bot, message):
 
 # Function to send image with custom message and inline button
 
-chat_id = message.from_user.id
+chat_id = message.chat.id
 def send_image_message(chat_id):
     Bot.send_photo(chat_id, photo=config.REPEAT_IMG, caption=config.REPEAT_TXT, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔥 Join Now 🔥", url=f"https://t.me/+oMv-bxaGMXVkNmE0")]]))
 
