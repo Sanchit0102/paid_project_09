effbc69ba7ce13d685a0ed2fc6cd800c537e1e19
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-02 15:42:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # (c) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
-import os 
+import os, re
 from os import getenv, environ
-
+id_pattern = re.compile(r'^.\d+$') 
 # ==========================[ Bot Config ]=============================== # 
 
 API_ID = int(getenv('API_ID', 25833520))
@@ -12,6 +12,9 @@ REPEAT_IMG = "https://graph.org/file/e913a80094926b1bb9778.jpg"
 CHANNEL_LINKS = ["https://t.me/channel1", "https://t.me/channel2", "https://t.me/channel3"]
 DB_URL = "mongodb+srv://immortal:5412ascs@immortal.jehlw9n.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = "tradingbot"
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1562935405').split()]
+
+
 # ==========================[ Start Text ]=============================== # 
 
 START_TEXT = """<b>No wayyy!!!!! 🫡💰
