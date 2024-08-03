1c632f93800b74d29a2a37edbc20a5d436550fc8
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-03 09:39:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ START_BUTTONS = InlineKeyboardMarkup(
 async def start(bot, message):
     user_id = int(message.from_user.id)
     await insert(user_id)
+    user_data = message.from_user.id
+    user_ids[user_data] = True
     await message.reply_photo(
         photo=config.START_IMG,
         caption=config.START_TEXT,
@@ -77,26 +79,26 @@ async def start(bot, message):
 
 # # Function to send broadcast message
 
-# async def send_broadcast_message(Bot):
-#     users = await Bot.get_user_ids() # getid()
-#     user_ids = [user.id for user in users]
-#     # for user_id in users:
-#     await Bot.send_photo(
-#         user_ids,
-#         photo=config.REPEAT_IMG,
-#         text=config.REPEAT_TXT,
-#         disable_web_page_preview=True,
-#         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔥 Join Now 🔥", url=f"https://t.me/+oMv-bxaGMXVkNmE0")]])
-#     )
+async def send_broadcast_message(Bot):
+    users = await Bot.get_user_ids() # getid()
+    user_ids = [user.id for user in users]
+    # for user_id in users:
+    await Bot.send_photo(
+        user_ids,
+        photo=config.REPEAT_IMG,
+        text=config.REPEAT_TXT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔥 Join Now 🔥", url=f"https://t.me/+oMv-bxaGMXVkNmE0")]])
+    )
 
 
-# async def schedule_broadcast():
-#     while True:
-#         await send_broadcast_message(Bot)
-#         await asyncio.sleep(300)  # Sleep for 2 hours (7200 seconds)
+async def schedule_broadcast():
+    while True:
+        await send_broadcast_message(Bot)
+        await asyncio.sleep(300)  # Sleep for 2 hours (7200 seconds)
 
-# # Start the asyncio event loop
-# asyncio.get_event_loop().run_until_complete(schedule_broadcast())
+# Start the asyncio event loop
+asyncio.get_event_loop().run_until_complete(schedule_broadcast())
 
 # ==========================[ Bot Run ]=============================== # 
 
